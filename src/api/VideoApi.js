a525aea284cee8836568b9417d9b99a2e0d66d97
Hemalatha7426 <Hemalatha7426>
2025-11-02 18:50:48 +0530

    complete

--- src/api/VideoApi.js
+++
@@ -1,4 +0,0 @@
-export async function getAllVideos() {
-const response = await fetch("https://hemspire-fullstack.onrender.com/api/videos");
-  return await response.json();
-}
--- src/api/audioApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_URL = "https://hemspire-fullstack.onrender.com/api/audio";
-
-export const getAllAudio = () => axios.get(API_URL);
-
-export const getAudioById = (id) => axios.get(`${API_URL}/${id}`);
-
-export const searchAudio = (keyword) => axios.get(`${API_URL}/search?keyword=${keyword}`);
